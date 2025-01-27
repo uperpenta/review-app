6ae5ce0d38985f5e97eebd496958b15ad5a410a1
Rares Ialomiteanu <rares-andrei.ialomiteanu@1and1.ro>
2025-01-27 13:18:40 +0200

    Final retouches

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,47 +1,7 @@
 import express from 'express';
 import connectDb from './db/mongoose';
 import {reviewRouter, commentRouter } from './routes/index';
-import { createReadStream } from 'fs';
-import path from 'path';
 import process from 'process';
-import google from 'googleapis';
-
-import pkey from './review-app-448709-3f9d4deb4441.json';
-
-
-
-//TODO upload in google drive
-//const SCOPES = [process.env.DRIVE];
-
-// async function authorize() {
-//     const jwtClient = new google.auth.JWT(
-//       pkey.client_email,
-//       null,
-//       pkey.private_key,
-//       SCOPES
-//     )
-//     await jwtClient.authorize();
-//     return jwtClient;
-//   }
-
-  /**
- * Create a new file on google drive.
- * @param {OAuth2Client} authClient An authorized OAuth2 client.
- */
-// async function uploadFile(authClient) {
-//     const drive = google.drive({ version: 'v3', auth: authClient });
-  
-//       const file = await drive.files.create({
-//         media: {
-//           body: createReadStream('filename')
-//         },
-//         fields: 'id',
-//         requestBody: {
-//           name: path.basename('filename'),
-//         },
-//       });
-//       console.log(file.data.id)
-//   }
 
 const port = process.env.PORT;
 
